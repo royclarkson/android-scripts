ce82f0e73edfe3210c8cef0b5da9fe3dfe2ea93d
Roy Clarkson <rclarkson@pivotal.io>
2014-10-20 12:42:36 -0500

    Add script to delete all emulators created by 'create-emulators' script

---
+++ delete-emulators.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function echoheading {
+    echo "-------------------"
+    echo " $1"
+    echo "-------------------"
+    echo 
+}
+
+function deleteavd {
+    android delete avd --name Android-$API
+    echo 
+    android delete avd --name Android-$API-x86
+    echo 
+}
+
+APIS=( 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 )
+for API in "${APIS[@]}"; do
+    echoheading "Delete Android-$API"
+    deleteavd $API
+done
